8f3d2bd5d409064b4fcd8592883a435728ab456a
Petter Johansson <pettjoha@kth.se>
2014-12-26 13:38:05 +0100

    Add module dataformats.read which reads a given file
    Calling 'read_flow_data(filename)' determines which file format type
    to use and calls the corresponding function

---
+++ dataformats/read.py
@@ -0,0 +1,82 @@
+import dataformats.simple as smp
+
+def guess_read_function(filename):
+    """Return a function handle to read a file.
+
+    Makes a guess on the data format by trying to access the file and assess
+    its characteristics, judging which of the implemented data formats is
+    the best match. Defaults to dataformats.simple.read_plainsimple if
+    file is not binary.
+
+    Args:
+        filename (str): File to read.
+
+    Returns:
+        function: Function handle.
+
+    """
+
+    def is_binary(filename, checksize=512):
+        """Return True if a file is binary-like, else False.
+
+        Tries to find a unicode '\n' character within checksize bytes
+        of the file and catches a UnicodeDecodeError if raised.
+
+        Args:
+            filename (str): File to control.
+
+            checksize (int): Number of bytes to control over.
+
+        Returns:
+            bool: Whether file is binary-like or not.
+
+        """
+
+        with open(filename, 'r') as fp:
+            try:
+                if '\n' in fp.read(checksize):
+                    return False
+                else:
+                    raise UnicodeDecodeError
+            except UnicodeDecodeError:
+                return True
+
+    if is_binary(filename):
+        function = smp.read_binsimple
+    else:
+        function = smp.read_plainsimple
+
+    return function
+
+
+def read_flow_data(filename):
+    """Return data and information about a flow field map.
+
+    Data and information are separate dict's returned as tuple. The data
+    dictionary structure is field names as keys and data in numpy.ndarrays
+    as values:
+
+        data = {'X': numpy.ndarray([0, 1, 2, ...]), ...}
+
+    The information dictionary always contains the same information:
+
+        info = {
+            'shape': tuple of number of bins in X and Y.
+            'size': dict with 'X' and 'Y' tuples of minimum
+                    and maximum positions along the axis.
+            'bin_size': tuple of bin size in X and Y.
+            'num_bins': number of bins in system.
+            }
+
+    Args:
+        filename (str): File to read data from.
+
+    Returns:
+        (dict, dict): data and information dictionaries.
+
+    """
+
+    function = guess_read_function(filename)
+    data, info = function(filename)
+
+    return data, info
---
+++ dataformats/tests/test_read.py
@@ -0,0 +1,27 @@
+from dataformats.read import *
+from dataformats.simple import *
+
+def test_guess_dataformat():
+    bin_filename = 'dataformats/tests/data_binsimple.dat'
+    plain_filename = 'dataformats/tests/data_plainsimple.dat'
+
+    assert (guess_read_function(bin_filename) == read_binsimple)
+    assert (guess_read_function(plain_filename) == read_plainsimple)
+
+def test_read_file():
+    filename = 'dataformats/tests/data_plainsimple.dat'
+    data, info = read_flow_data(filename)
+
+    # Control values for simple file
+    NUM_BINS = 12
+    NUM_X, NUM_Y = 3, 4
+    MIN_X, MAX_X = 133.125, 133.625
+    MIN_Y, MAX_Y = 0.375, 1.125
+    BIN_X, BIN_Y = 0.25, 0.25
+
+    # Verify that plaintext file was read
+    assert (info['num_bins'] == NUM_BINS)
+    assert (info['shape'] == [NUM_X, NUM_Y])
+    assert (info['size']['X'] == [MIN_X, MAX_X])
+    assert (info['size']['Y'] == [MIN_Y, MAX_Y])
+    assert (info['bin_size'] == [BIN_X, BIN_Y])
