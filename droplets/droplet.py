e04c562c7159f35eb71fed3fea91ec68ecf63ec2
Petter Johansson <pettjoha@kth.se>
2015-01-19 19:13:21 +0100

    Change interface search algorithm
    Instead of using the uncertain and weird (albeit maybe fast)
    contour function of matplotlib the new algorithm scans the system
    layer by layer for boundary cells, determined through how many
    neighbouring cells (and if itself) pass a cut-off value which
    defaults to the midpoint between the max and min values of the
    droplet parameter in the system.
    
    For each layer the edges are yielded as a 2-tuple, which means
    that just a few layers can be retrieved if desired, instead of
    searching through the entire data structure at once. This
    should make implementing a spreading radius function trivial.

--- droplets/droplet.py
+++ droplets/droplet.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def get_droplet_interface(flow, label, cutoff=None, coord_labels=('X', 'Y')):
-    """Return droplet interface coordinates.
 
-    Currently works only with a fully initialised, rectangular system.
-    Returns the longest contour found in the system.
+def get_interface(flow, label, radius, **kwargs):
+    """Yield droplet interface indices of a FlowData object.
+
+    Indices are yielded from bottom to the top and correspond to found
+    left and right indices of the boundary.
 
     Args:
         flow (FlowData): A FlowData object. Must contain a data record
@@ -14,45 +15,125 @@ def get_droplet_interface(flow, label, cutoff=None, coord_labels=('X', 'Y')):
 
         label (str): Record label used as base for the interface height map.
 
-        cutoff (float, optional): Which interface height to cut the contour at.
-            Defaults to the halfway height.
+        radius (float): Radius to include cells within.
 
-        coord_labels (2-tuple, optional): Record labels for coordinates.
+    Keyword Args:
+        cutoff (float, default=None): Which interface height to cut the
+            boundary at. Defaults to the midpoint height.
 
-    Returns:
-        ndarray: Array with 2-tuples of interface coordinates.
+        num_cells (int, default=1): Number of cells inside the set radius
+            which must pass the cut-off criteria.
 
-    Raises:
-        AttributeError: If no unique contour was found.
+        coord_labels (2-tuple, default=('X', 'Y'): Record labels for coordinates.
+
+    Yields:
+        int, int: Indices for the left and right boundary of an
+            interface layer.
 
     """
 
-    def get_levels(cutoff):
-        if cutoff != None:
-            return [cutoff if cutoff > hdata.min() else hdata.min()]
+    def traverse_layer(sorted_layer):
+        for cell in sorted_layer:
+            index = np.where(flow.data == cell)[0][0]
+
+            if cell_is_droplet(index, flow.data, label, radius, **kwargs):
+                return index
+
         else:
-            return 1
+            return None
 
-    def get_vertices(paths):
-        if paths == []:
-            return paths
+    Y = flow.data['Y']
+    ys = np.unique(Y)
 
-        lengths = np.array([len(apath.vertices) for apath in paths])
-        if len(lengths == lengths.max()) == 1:
-            return paths[lengths.argmax()].vertices
+    for i, y in enumerate(ys):
+        layer = flow.data[Y == y]
+        sorted_layer = np.sort(layer)
+
+        left = traverse_layer(sorted_layer)
+        right = traverse_layer(reversed(sorted_layer))
+
+        if left != None and right != None:
+            yield left, right
+
+
+def cell_is_droplet(cell, system, label, radius, **kwargs):
+    """Determine whether a cell is a connected part of a liquid system.
+
+    The simple rule is that the cell and a given number of cells within
+    a radius of it must have a parameter value larger than or equal to
+    a cut-off value.
+
+    Periodic boundary conditions are not applied for the radius search.
+
+    Args:
+        cell (int): Index of origin cell in system record array.
+
+        system (record): Field data in record format.
+
+        label (str): Record label used as base for the boundary determination.
+
+        radius (float): Radius to include cells within.
+
+    Keyword Args:
+        cutoff (float, default=None): Which interface height to cut the
+            boundary at. Defaults to the midpoint height.
+
+        num_cells (int, default=1): Number of cells inside the set radius
+            which must pass the cut-off criteria.
+
+        coord_labels (2-tuple, default=('X', 'Y'): Record labels for coordinates.
+
+    Returns:
+        bool: Whether or not the cell can be considered part of the liquid.
+
+    Raises:
+        IndexError: If coordinate labels could not be found in data record.
+
+    """
+
+    cutoff = kwargs.pop('cutoff', None)
+    num_cells = kwargs.pop('num_cells', 1)
+
+    try:
+        if cutoff == None:
+            cutoff = 0.5*(np.max(system[label]) + np.min(system[label]))
+
+        try:
+            assert (system[cell][label] >= cutoff)
+            indices = get_indices_in_radius(cell, system, radius, **kwargs)
+            assert (len(system[system[label][indices] >= cutoff]) >= num_cells)
+        except AssertionError:
+            return False
         else:
-            raise AttributeError("could not find unique contour as interface")
+            return True
 
-    # Get shaped view of system
-    shape = flow.shape
-    data = flow.data.reshape(shape)
+    except IndexError:
+        raise IndexError("could not find set labels in system")
 
-    x, y = [data[clabel] for clabel in coord_labels]
-    hdata = data[label]
 
-    # Get one contour level from the data
-    levels = get_levels(cutoff)
-    cx = plt.contour(x, y, hdata, levels)
-    paths = cx.collections[0].get_paths()
+def get_indices_in_radius(cell, system, radius, **kwargs):
+    """Return indices of cells within a given radius of input cell.
 
-    return get_vertices(paths)
+    Args:
+        cell (int): Index of origin cell in system record array.
+
+        system (record): Field data in record format.
+
+        radius (float): Radius to include cells within.
+
+    Keyword Args:
+        coord_labels (2-tuple, default=('X', 'Y'): Record labels for coordinates.
+
+    Returns:
+        ndarray: Numpy array of indices of cells within set radius of origin.
+
+    """
+
+    coord_labels = kwargs.pop('coord_labels', ('X', 'Y'))
+
+    x, y = (system[cell][coord_labels[i]] for i in (0, 1))
+    xs, ys = (system[coord_labels[i]] for i in (0, 1))
+
+    indices = np.where((np.sqrt((xs - x)**2 + (ys - y)**2) <= radius))[0]
+
+    return indices[indices != cell]
--- droplets/tests/test_droplet.py
+++ droplets/tests/test_droplet.py
@@ -8,30 +8,175 @@ x = np.arange(11)
 y = np.arange(11)
 xs, ys = np.meshgrid(x, y)
 info = {'shape': (11,11)}
+dtype = [(l, 'float32') for l in ('X', 'Y', 'C')]
 
 # Create droplet centered at (5,5)
 mid, spread = 5, 10
 cs = np.exp(-((xs-mid)**2 + (ys-mid)**2)/spread)
 
+# System
+system = np.zeros(xs.size, dtype=dtype).ravel()
+system['X'] = xs.ravel()
+system['Y'] = ys.ravel()
+system['C'] = cs.ravel()
+
+def test_find_indices_in_radius():
+    cell = np.floor(system.size/2)
+
+    radius = 1 - 1e-6
+    indices = get_indices_in_radius(cell, system, radius)
+    assert (len(indices) == 0)
+
+    radius = 1
+    indices = get_indices_in_radius(cell, system, radius)
+    assert (set(indices) == set([49, 59, 61, 71]))
+
+    radius = 1.5
+    indices = get_indices_in_radius(cell, system, radius)
+    assert (set(indices) == set([48, 49, 50, 59, 61, 70, 71, 72]))
+
+def test_find_indices_in_radius_general_names():
+    dtype = [(l, 'float32') for l in ('f0', 'f1', 'C')]
+    system = np.zeros(xs.size, dtype=dtype).ravel()
+    system['f0'] = xs.ravel()
+    system['f1'] = ys.ravel()
+
+    cell = np.floor(system.size/2)
+
+    radius = 1
+    indices = get_indices_in_radius(cell, system, radius, coord_labels=('f0', 'f1'))
+    assert (set(indices) == set([49, 59, 61, 71]))
+
+def test_cell_is_droplet():
+    datasize = 5
+    system = np.zeros(datasize, dtype=dtype).ravel()
+    system['X'] = np.arange(datasize)
+    system['Y'] = np.arange(datasize)
+    system['C'][[1,2,3]] = 1
+
+    label = 'C'
+    radius = 1.5
+
+    droplet = []
+    for cell in np.arange(system.size):
+        droplet.append(cell_is_droplet(cell, system, label, radius))
+    assert (droplet == [False, True, True, True, False])
+
+def test_cell_is_droplet_cutoff():
+    datasize = 5
+    system = np.zeros(datasize, dtype=dtype).ravel()
+    system['X'] = np.arange(datasize)
+    system['Y'] = np.arange(datasize)
+    system['C'][[1,2,3]] = 1
+
+    label = 'C'
+    radius = 1.5
+
+    droplet = []
+    cutoff = 2
+    for cell in np.arange(system.size):
+        droplet.append(cell_is_droplet(cell, system, label, radius, cutoff=cutoff))
+    assert (droplet == [False, False, False, False, False])
+
+    droplet = []
+    cutoff = 0.5
+    for cell in np.arange(system.size):
+        droplet.append(cell_is_droplet(cell, system, label, radius, cutoff=cutoff))
+    assert (droplet == [False, True, True, True, False])
+
+
+
+def test_cell_is_droplet_numcells():
+    datasize = 5
+    system = np.zeros(datasize, dtype=dtype).ravel()
+    system['X'] = np.arange(datasize)
+    system['Y'] = np.arange(datasize)
+    system['C'][[1,2,3]] = 1
+
+    label = 'C'
+    radius = 1.5
+    num_cells = 2
+
+    droplet = []
+    for cell in np.arange(system.size):
+        droplet.append(cell_is_droplet(cell, system, label, radius,
+            num_cells=num_cells))
+    assert (droplet == [False, False, True, False, False])
+
+def test_cell_is_droplet_general_names():
+    datasize = 5
+    dtype = [(l, 'float32') for l in ('f0', 'f1', 'C')]
+    system = np.zeros(datasize, dtype=dtype).ravel()
+    system['f0'] = np.arange(datasize)
+    system['f1'] = np.arange(datasize)
+    system['C'][[1,2,3]] = 1
+
+    coord_labels = ('f0', 'f1')
+    label = 'C'
+    cutoff = 1
+    radius = 1.5
+
+    droplet = []
+    for cell in np.arange(system.size):
+        droplet.append(cell_is_droplet(cell, system, label, radius,
+            coord_labels=coord_labels))
+    assert (droplet == [False, True, True, True, False])
+
+def test_cell_is_droplet_bad_names():
+    datasize = 1
+    dtype = [(l, 'float32') for l in ('f0', 'f1', 'C')]
+    system = np.zeros(datasize, dtype=dtype).ravel()
+    system['f0'] = np.arange(datasize)
+    system['f1'] = np.arange(datasize)
+
+    coord_labels = ('c0', 'c1')
+    label = 'C'
+    radius = 1.5
+
+    with pytest.raises(IndexError):
+        cell_is_droplet(0, system, label, radius)
+
+def test_find_interface_bottom():
+    datasize = 5
+    system = {}
+    system['X'] = np.arange(datasize)
+    system['Y'] = np.zeros(datasize)
+    system['C'] = np.array([0, 1, 1, 1, 0])
+    flow = FlowData(system)
+
+    label = 'C'
+    radius = 1.5
+
+    for i, (ileft, iright) in enumerate(get_interface(flow, label, radius)):
+        assert (ileft == 1 and iright == 3)
+        break
+
+    num_cells = 2
+    for ileft, iright in get_interface(flow, label, radius, num_cells=num_cells):
+        assert (ileft == 2 and iright == 2)
+        break
+
 def test_find_interface():
-    flow = FlowData(('X', xs), ('Y', ys), ('C', cs), info=info)
-    interface = get_droplet_interface(flow, 'C')
-    assert (np.shape(interface) == (21,2))
+    flow = FlowData(('X', xs), ('Y', ys), ('C', cs))
 
-def test_find_interface_cutoffs():
-    flow = FlowData(('X', xs), ('Y', ys), ('C', cs), info=info)
+    label = 'C'
+    radius = 1.5
 
-    interface = get_droplet_interface(flow, 'C', cutoff=0.8)
-    assert (np.shape(interface) == (13,2))
+    interface = []
+    for i, (ileft, iright) in enumerate(get_interface(flow, label, radius)):
+        interface.append(flow.data[[ileft, iright]])
+    assert (len(interface) == 5)
 
-    interface = get_droplet_interface(flow, 'C', cutoff=5)
-    assert (np.shape(interface) == (0,))
+    interface = []
+    cutoff = 0.2
+    for i, (ileft, iright) in enumerate(get_interface(flow, label, radius,
+            cutoff=cutoff)):
+        interface.append(flow.data[[ileft, iright]])
+    assert (len(interface) == 9)
 
-    with pytest.raises(AttributeError):
-        interface = get_droplet_interface(flow, 'C', cutoff=-1)
-
-def test_find_interface_general_names():
-    flow = FlowData(('f0', xs), ('f1', ys), ('f2', cs), info=info)
-    labels = ('f0', 'f1')
-    interface = get_droplet_interface(flow, 'f2', None, coord_labels=labels)
-    assert (np.shape(interface) == (21,2))
+    interface = []
+    cutoff = 1.1
+    for i, (ileft, iright) in enumerate(get_interface(flow, label, radius,
+            cutoff=cutoff)):
+        interface.append(flow.data[[ileft, iright]])
+    assert (len(interface) == 0)
