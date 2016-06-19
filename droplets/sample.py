droplets sample: correct viscous dissipation calculation formula
By mistake this did not square the divergence term, making things fully
incorrect. Tricky to test against since there is no easy logic to
perform except the formula itself.

droplets/sample.py
@@ -129,6 +129,6 @@ def sample_viscous_dissipation(flow, viscosity,
     dudy, dudx = np.gradient(U, dy, dx, edge_order=2)
     dvdy, dvdx = np.gradient(V, dy, dx, edge_order=2)
 
-    return 2*viscosity*(dudx**2 + dvdy**2 - (dudx + dvdy)/3.0) \
+    return 2*viscosity*(dudx**2 + dvdy**2 - (dudx + dvdy)**2/3.0) \
             + viscosity*(dvdx + dudy)**2
 

droplets/tests/test_sample_viscous_dissipation.py
@@ -29,7 +29,7 @@ def test_calc_viscous_dissipation():
     dvdx = (vs[2, 2] - vs[2, 0])/2
     dvdy = np.gradient(vs[:, 1], 0.5, edge_order=2)[2]
 
-    visc_diss = 2*viscosity*(dudx**2 + dvdy**2 - (dudx + dvdy)/3.0) \
+    visc_diss = 2*viscosity*(dudx**2 + dvdy**2 - (dudx + dvdy)**2/3.0) \
             + viscosity*(dvdx + dudy)**2
 
     assert np.isclose(visc_diss, result[2, 1])
@@ -56,7 +56,7 @@ def test_calc_viscous_dissipation_other_labels():
     dudy, dudx = np.gradient(us, edge_order=2)
     dvdy, dvdx = np.gradient(vs, edge_order=2)
 
-    visc_diss = 2*viscosity*(dudx**2 + dvdy**2 - (dudx + dvdy)/3.0) \
+    visc_diss = 2*viscosity*(dudx**2 + dvdy**2 - (dudx + dvdy)**2/3.0) \
             + viscosity*(dvdx + dudy)**2
 
     assert np.isclose(visc_diss, result).all()
@@ -87,7 +87,7 @@ def test_calc_viscous_dissipation_unsorted_bins():
     dudy, dudx = np.gradient(us, edge_order=2)
     dvdy, dvdx = np.gradient(vs, edge_order=2)
 
-    visc_diss = 2*viscosity*(dudx**2 + dvdy**2 - (dudx + dvdy)/3.0) \
+    visc_diss = 2*viscosity*(dudx**2 + dvdy**2 - (dudx + dvdy)**2/3.0) \
             + viscosity*(dvdx + dudy)**2
 
     assert np.isclose(visc_diss, result).all()