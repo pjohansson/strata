strata average: fix recentering and combining cells
Recentering now recalculates the information dict which is needed to
combine the cells.

strata/average.py
@@ -71,7 +71,6 @@ def average(base, output, group=1, **kwargs):
     cutoff_radius = kwargs.pop('cutoff_radius', 1.)
 
     combine = kwargs.pop('combine', (None, None))
-    print(combine)
     if combine != (None, None):
         nx, ny = int(combine[0]), int(combine[1])
 
@@ -114,8 +113,7 @@ def average(base, output, group=1, **kwargs):
             else:
                 raise ValueError("Invalid edge to recenter around ('%s'). Must be 'left' or 'right'" % recenter)
 
-
-            group_data = recenter_maps(group_data, xs_edges)
+            group_data, info = recenter_maps(group_data, xs_edges)
 
         avg_data = module.average_data(*group_data)
 
@@ -175,4 +173,19 @@ def recenter_maps(data_maps, recenter_values):
 
         recentered_data.append(data_subset)
 
-    return recentered_data
+    # Reconstruct info dict, since the coordinate grid has to be identical
+    # for all final maps we can cheat a little bit
+    origin = [xmin, np.min(data_subset['Y'])]
+    spacing = [spacing, np.mean(np.diff(np.unique(data_subset['Y'])))]
+    nx = int((xmax - xmin)/spacing[0]) + 1
+    num_bins = data_subset['X'].size
+    shape = [nx, num_bins // nx]
+
+    info = {
+        'origin': origin,
+        'spacing': spacing,
+        'shape': shape,
+        'num_bins': num_bins
+    }
+
+    return recentered_data, info

strata/dataformats/simple/average.py
@@ -128,8 +128,8 @@ def combine_bins(data, info, nx, ny):
 
         for i in range(shape[0]):
             for j in range(shape[1]):
-                if ws is None:
-                    new_data[label][i, j] = np.mean(ds[nx*i:nx*(i+1), ny*j:ny*(j+1)])
+                if ws is weight:
+                    new_data[label][i, j] = np.sum(ds[nx*i:nx*(i+1), ny*j:ny*(j+1)])
                 else:
                     try:
                         new_data[label][i, j] = np.average(ds[nx*i:nx*(i+1), ny*j:ny*(j+1)],

strata/dataformats/simple/tests/test_combine_simple.py
@@ -33,8 +33,12 @@ def test_combine_bins():
     assert(np.array_equal([1.0], new_data['Y']))
 
     for label, weight in labels_and_weights:
+        # The number of atoms and their mass is summed as the bins are
+        # combined. The flow and the temperature of the bins are averaged.
+        # This is taken care of by the weight labels since all the quantities
+        # that are averaged have a weight, while the others do not.
         if weight == None:
-            assert(np.allclose([data[label].mean()], new_data[label]))
+            assert(np.allclose([data[label].sum()], new_data[label]))
         # As per above 'T' will average to 0.0 due to the weight summing to it
         elif label != 'T':
             assert(np.allclose([np.average(data[label], weights=data[weight])], new_data[label]))
@@ -62,8 +66,8 @@ def test_read_and_combine_bins():
     for label, weight in labels_and_weights:
         ds = data[label].reshape(info['shape'])
         if weight == None:
-            assert(np.isclose(ds[0:3, 0:2].mean(), combined_data[label][0]))
-            assert(np.isclose(ds[0:3, 2:].mean(), combined_data[label][1]))
+            assert(np.isclose(ds[0:3, 0:2].sum(), combined_data[label][0]))
+            assert(np.isclose(ds[0:3, 2:].sum(), combined_data[label][1]))
         else:
             ws = data[weight].reshape(info['shape'])
             assert(np.isclose(np.average(ds[0:3, 0:2], weights=ws[0:3, 0:2]), combined_data[label][0]))