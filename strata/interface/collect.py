9edd03c52539809d7e90f7472d4e769f6261eb30
Petter Johansson <pettjoha@kth.se>
2018-05-16 16:55:45 +0200

    Update interface collection to account for PBC crossing

--- strata/interface/collect.py
+++ strata/interface/collect.py
@@ -3,9 +3,13 @@ import os
 import pandas as pd
 import progressbar as pbar
 
+from collections import namedtuple
+
 from droplets.flow import FlowData
 from droplets.interface import get_interface
 from strata.dataformats.read import read_data_file
+from strata.spreading.collect import init_periodic_info, \
+    check_and_update_periodic_info, add_pbc_multipliers_to_edges, PeriodicInfo
 from strata.utils import find_singles_to_singles, pop_fileopts, prepare_path
 
 
@@ -29,9 +33,6 @@ def collect_interfaces(base, output, recenter=None, **kwargs):
 
         outext (str, default='.xvg.'): Output file extension.
 
-    Returns:
-        xs, ys: 2-tuple with lists of all interface coordinates along x and y.
-
     See `droplets.interface.get_interface` for more keyword arguments.
 
     """
@@ -48,7 +49,9 @@ def collect_interfaces(base, output, recenter=None, **kwargs):
     quiet = kwargs.pop('quiet', False)
 
     files = list(find_singles_to_singles(base, output, **fopts))
-    xs, ys = [], []
+
+    pbc_info_per_y = None
+    yindex_data = None
 
     if not quiet:
         widgets = ['Collecting from files: ',
@@ -57,23 +60,248 @@ def collect_interfaces(base, output, recenter=None, **kwargs):
         progress.start()
 
     for i, (fn, fnout) in enumerate(files):
-        data, _, _ = read_data_file(fn)
-        flow = FlowData(data)
-        x, y = get_interface_coordinates(flow, label, recenter, **kwargs)
-        interface = pd.Series(x, index=y)
+        data, info, _ = read_data_file(fn)
+        flow = FlowData(data, info=info)
+        interface, pbc_info_per_y, yindex_data = get_interface_coordinates(
+            flow, label, pbc_info_per_y, yindex_data, recenter, **kwargs
+        )
 
         write_interface_data(fnout, interface, [fnout], kwargs, recenter)
 
-        xs.append(interface.values)
-        ys.append(interface.index)
-
         if not quiet:
-            progress.update(i+1)
+            progress.update(i + 1)
 
     if not quiet:
         progress.finish()
 
-    return xs, ys
+
+def get_interface_coordinates(flow, label, pbc_info_per_y, yindex_data,
+        recenter=None, **kwargs):
+    """Get the interface from a FlowData object using input label.
+
+    Args:
+        flow (FlowData): A FlowData object. Must contain a data record
+            with coordinates and selected height map label.
+
+        label (str): Record label used as base for the interface height map.
+
+        pbc_info_per_y (PeriodicInfo): Array of current pbc information for
+            every y-value in the previous interface.
+
+        yindex_data (YIndexData): Information about the indexing for the
+            `pbc_info_per_y` array per y-value.
+
+    Keyword Args:
+        recenter (str, optional): Recenter the interface around 'zero' or
+            the center of mass 'com'.
+
+        coord_labels (2-tuple, default=('X', 'Y'): Record labels for coordinates.
+
+    Returns:
+        ndarray, ndarray, [PeriodicInfo], YIndexData: 2-tuple with interfaces coordinates along x and y, the updated periodic information and indexing.
+
+    See `get_interface` for additional keyword arguments.
+
+    """
+
+    xl, yl = kwargs.get('coord_labels', ('X', 'Y'))
+
+    left, right = np.array(list(
+        get_interface(flow, label, search_longest_connected=True, **kwargs)
+    )).T
+
+    ys = flow.data[yl][left]
+    xs_left = flow.data[xl][left]
+    xs_right = flow.data[xl][right]
+
+    box_x, _ = flow.size()
+
+    interface, pbc_info_per_y, yindex_data = update_interface_with_pbc_info(
+        ys, xs_left, xs_right, box_x, pbc_info_per_y, yindex_data
+    )
+
+    xs = interface['X']
+    ys = interface['Y']
+
+    ys -= ys[0]
+
+    if recenter == 'zero':
+        xs -= np.mean([xs[0], xs[-1]])
+    elif recenter == 'com':
+        xs -= np.average(flow.data[xl], weights=flow.data[label])
+
+    return interface, pbc_info_per_y, yindex_data
+
+
+# Data for indexing a vector for floating point y-values.
+YIndexData = namedtuple("YIndexData", ["ymin", "ymax", "dy", "num"])
+
+def get_yindex_data(ys):
+    """Return the `YIndexData` corresponding to the input valuesself.
+
+    Note that this assumes that all the values in the input array are
+    evenly spaced.
+
+    """
+
+    dy = np.unique(np.diff(ys))[0]
+
+    return YIndexData(
+        ymin=np.min(ys),
+        ymax=np.max(ys),
+        dy=dy,
+        num=ys.size,
+    )
+
+def get_yindex_for_value(y, yindex_data):
+    """For a given `YIndexData`, return the index corresponding to a value.
+
+    Values outside of the minimum and maximum values are clipped to the edges.
+
+    """
+
+    y = min(max(y, yindex_data.ymin), yindex_data.ymax)
+    return int((y - yindex_data.ymin) / yindex_data.dy)
+
+def transfer_values(xs, yindex_data, yindex_data_new):
+    """Transfer data from an array to one with a new indexing.
+
+    If `yindex_data` is `None`, an array matching `yindex_data_new` is
+    returned filled with `None` values. In this case, `xs` can also be
+    `None`, since it is not used.
+
+    The idea is that we can have interface data for different y-values
+    in every frame, but want to keep track of their PBC information to
+    adjust the final coordinates. Thus we need some array that can merge
+    these different sets.
+
+    The indices which did not have a data in the input array are set to `None`.
+
+    Args:
+        xs (ndarray): Values corresponding to the `yindex_data` indexing.
+
+        yindex_data (YIndexData): Indexing for the input data.
+
+        yindex_data_new (YIndexData): Indexing for the output data.
+
+    Returns:
+        ndarray: An array matching the `yindex_data_new` object with the
+            values from the input array set in their corresponding indices.
+
+    Excepts:
+        ValueError: If the input `YIndexData` objects do not have identical
+            spacing `dy`.
+
+    """
+
+    xs_new = np.array([None for _ in range(yindex_data_new.num)])
+
+    if yindex_data == None:
+        return xs_new
+
+    # Just return the array if the metadata is identical
+    if np.isclose(yindex_data.ymin, yindex_data_new.ymin) \
+            and np.isclose(yindex_data.ymax, yindex_data_new.ymax) \
+            and np.isclose(yindex_data.dy, yindex_data_new.dy) \
+            and yindex_data.num == yindex_data_new.num:
+        return xs
+
+    if not np.isclose(yindex_data.dy, yindex_data_new.dy):
+        raise ValueError(
+                "transfer of values is not implemented for different dy"
+            )
+
+    imin = get_yindex_for_value(yindex_data.ymin, yindex_data_new)
+    imax = get_yindex_for_value(yindex_data.ymax, yindex_data_new)
+
+    jmin = get_yindex_for_value(yindex_data_new.ymin, yindex_data)
+    jmax = get_yindex_for_value(yindex_data_new.ymax, yindex_data)
+
+    xs_new[imin:imax + 1] = xs[jmin:jmax + 1]
+
+    return xs_new
+
+def create_interface_array(ys, xs_left, xs_right):
+    """Merge the left and right interface edges at the top."""
+
+    interface = np.zeros(
+        (2 * ys.size, ), dtype=[('Y', np.float), ('X', np.float)]
+    )
+
+    interface['Y'][:ys.size] = ys
+    interface['Y'][ys.size:] = ys[::-1]
+    interface['X'][:ys.size] = xs_left
+    interface['X'][ys.size:] = xs_right[::-1]
+
+    return interface
+
+def fill_pbc_info_for_empty_y_values(pbc_info_per_y):
+    """Return the input array with `None` values replaced by copies of others.
+
+    The copies are taken towards the inner part, ie. from the bottom and from
+    the top depending on where the `None` value is. The center is assumed
+    to have no gaps.
+
+    If no `PeriodicInfo` objects exist in the array, all values are set to
+    the default `PeriodicInfo` object.
+
+    """
+
+    try:
+        first = 0
+        while (pbc_info_per_y[first] == None):
+            first += 1
+    except:
+        pbc_info = init_periodic_info()
+        pbc_info_per_y = [pbc_info] * len(pbc_info_per_y)
+    else:
+        for i in range(first):
+            pbc_info_per_y[i] = pbc_info_per_y[first]
+
+        last = -1
+        while (pbc_info_per_y[last] == None):
+            last -= 1
+
+        for i in range(-1, last, -1):
+            pbc_info_per_y[i] = pbc_info_per_y[last]
+
+    return pbc_info_per_y
+
+def update_interface_with_pbc_info(ys, xs_left, xs_right, box_x,
+        old_pbc_info_per_y, old_yindex_data):
+    """Check whether the interface jumps for any y-value and update pbc info."""
+
+    new_yindex_data = get_yindex_data(ys)
+    new_pbc_info_per_y = transfer_values(
+        old_pbc_info_per_y, old_yindex_data, new_yindex_data
+    )
+    new_pbc_info_per_y = fill_pbc_info_for_empty_y_values(new_pbc_info_per_y)
+
+    for i, (y, xl, xr) in enumerate(zip(ys, xs_left, xs_right)):
+        pbc_info = new_pbc_info_per_y[i]
+
+        if pbc_info != None:
+            new_pbc_info = check_and_update_periodic_info(
+                pbc_info, box_x, xl, xr
+            )
+        else:
+            new_pbc_info = init_periodic_info(xleft=xl, xright=xr)
+
+        new_pbc_info_per_y[i] = new_pbc_info
+
+    final_xs_left = np.zeros(xs_left.shape)
+    final_xs_right = np.zeros(xs_right.shape)
+
+    for i, (xl, xr, pbc_info) in enumerate(zip(
+                xs_left, xs_right, new_pbc_info_per_y
+            )):
+        xleft, xright = add_pbc_multipliers_to_edges(pbc_info, box_x, xl, xr)
+        final_xs_left[i] = xleft
+        final_xs_right[i] = xright
+
+    interface = create_interface_array(ys, final_xs_left, final_xs_right)
+
+    return interface, new_pbc_info_per_y, new_yindex_data
 
 
 @prepare_path
@@ -86,7 +314,7 @@ def write_interface_data(path, interface, fngroup, kwargs, recenter=None):
     Args:
         path (str): Path to output file.
 
-        interface (Series): Interface data.
+        interface (ndarray): Interface data.
 
         fngroup (list): List of data files read from.
 
@@ -136,44 +364,6 @@ def write_interface_data(path, interface, fngroup, kwargs, recenter=None):
         return header + inputs
 
     header = get_header(path, fngroup)
-    data = np.array([interface, interface.index]).T
+    data = np.array([interface['X'], interface['Y']]).T
     np.savetxt(path, data, fmt='%.3f', delimiter=' ',
             header=header, comments='')
-
-
-def get_interface_coordinates(flow, label, recenter=None, **kwargs):
-    """Get the interface from a FlowData object using input label.
-
-    Args:
-        flow (FlowData): A FlowData object. Must contain a data record
-            with coordinates and selected height map label.
-
-        label (str): Record label used as base for the interface height map.
-
-    Keyword Args:
-        recenter (str, optional): Recenter the interface around 'zero' or
-            the center of mass 'com'.
-
-        coord_labels (2-tuple, default=('X', 'Y'): Record labels for coordinates.
-
-    Returns:
-        ndarray, ndarray: 2-tuple with interfaces coordinates along x and y.
-
-    See `get_interface` for additional keyword arguments.
-
-    """
-
-    xl, yl = kwargs.get('coord_labels', ('X', 'Y'))
-
-    left, right = np.array(list(get_interface(flow, label, **kwargs))).T
-    inds = np.array(left.tolist() + right[::-1].tolist())
-
-    xs, ys = (flow.data[l][inds] for l in (xl, yl))
-    ys -= ys[0]
-
-    if recenter == 'zero':
-        xs -= np.mean([xs[0], xs[-1]])
-    elif recenter == 'com':
-        xs -= np.average(flow.data[xl], weights=flow.data[label])
-
-    return xs, ys
--- strata/interface/tests/test_collect_interface.py
+++ strata/interface/tests/test_collect_interface.py
@@ -1,11 +1,14 @@
 import numpy as np
 import os
+import pytest
 import tempfile as tmp
 
 from droplets.flow import FlowData
 from droplets.interface import get_interface
 from strata.interface.collect import *
 from strata.dataformats.write import write, flowdata_to_dict
+from strata.spreading.collect import init_periodic_info, \
+    check_and_update_periodic_info, add_pbc_multipliers_to_edges, PeriodicInfo
 from strata.utils import gen_filenames, find_datamap_files
 
 coords = np.linspace(0, 10, 11)
@@ -14,10 +17,13 @@ cs = np.zeros(xs.shape)
 cs[:,1:10] = 1.
 
 data = {'X': xs, 'Y': ys, 'C': cs}
-flow = FlowData(data)
+info = {'shape': (11, 11), 'spacing': (1., 1.)}
+flow = FlowData(data, info=info)
 
 def test_get_interface_coords():
-    x, y = get_interface_coordinates(flow, 'C')
+    interface, _, _ = get_interface_coordinates(flow, 'C', None, None)
+    x = interface['X']
+    y = interface['Y']
 
     assert np.array_equiv(x[:len(coords)], 1.)
     assert np.array_equiv(x[len(coords):], 9.)
@@ -26,7 +32,9 @@ def test_get_interface_coords():
 
 def test_adjust_interface_to_com():
     com = np.average(flow.data['X'], weights=flow.data['C'])
-    x, y = get_interface_coordinates(flow, 'C', recenter='com')
+    interface, _, _ = get_interface_coordinates(flow, 'C', None, None, recenter='com')
+    x = interface['X']
+    y = interface['Y']
 
     assert np.isclose(com, 5.)
     assert np.array_equiv(x[:len(coords)], 1.-com)
@@ -34,21 +42,186 @@ def test_adjust_interface_to_com():
     assert np.array_equal(y[:len(coords)], coords)
     assert np.array_equal(y[len(coords):], coords[::-1])
 
-def test_read_interface():
-    empty = np.empty_like(xs)
-    data = {'X': xs, 'Y': ys, 'M': cs,
-            'U': empty, 'V': empty, 'N': empty, 'T': empty
-            }
+def init_pbc_info_with_positions(xleft, xright):
+    return PeriodicInfo(
+        xleft_prev=xleft,
+        xright_prev=xright,
+        pbc_multiplier_left=0,
+        pbc_multiplier_right=0,
+    )
 
-    flow = FlowData(data)
-    test = get_interface_coordinates(flow, 'M', adjust_com=True)
-    x, y = test
+def test_get_initial_yvalue_meta_data():
+    ys = 1. + np.arange(10.) / 9.
+    yindex = get_yindex_data(ys)
 
-    with tmp.TemporaryDirectory() as tmpdir:
-        fnbase = os.path.join(tmpdir, 'test_')
-        path = next(gen_filenames(fnbase))
-        write(path, flowdata_to_dict(flow))
+    assert (yindex.ymin == 1.)
+    assert (yindex.ymax == 1. + (10. - 1.) / 9.)
+    assert (np.isclose(yindex.dy, 1. / 9.))
+    assert (yindex.num == 10)
 
-        xs_list, ys_list = collect_interfaces(fnbase, os.path.join(tmpdir,''))
-        assert (np.array_equal(xs_list[0], x))
-        assert (np.array_equal(ys_list[0], y))
+def test_get_yvalue_indices_using_meta_data():
+    ys = 1. + np.arange(10.)
+    yindex = get_yindex_data(ys)
+
+    assert (get_yindex_for_value(1.0, yindex) == 0)
+    assert (get_yindex_for_value(2.0, yindex) == 1)
+    assert (get_yindex_for_value(3.0, yindex) == 2)
+    assert (get_yindex_for_value(10.0, yindex) == 9)
+
+def test_get_yvalue_indices_for_out_of_bounds_data_is_edges():
+    ys = 1. + np.arange(10.)
+    yindex = get_yindex_data(ys)
+
+    assert (get_yindex_for_value(0.0, yindex) == 0)
+    assert (get_yindex_for_value(11.0, yindex) == 9)
+
+def test_get_yvalue_indices_rounds_to_closest_match():
+    ys = 1. + np.arange(10.) / 9.
+    yindex = get_yindex_data(ys)
+
+    assert (get_yindex_for_value(1. + 1. / 9. - 1e-3, yindex) == 0)
+    assert (get_yindex_for_value(1. + 1. / 9., yindex) == 1)
+    assert (get_yindex_for_value(1. + 1. / 9. + 1e-3, yindex) == 1)
+
+def test_create_superset_of_yvalues_with_none_when_getting_larger_meta_data():
+    ys_old = 2. + np.arange(5.) # [2., 6.]
+    ys_new = np.arange(9.)      # [0., 8.]
+
+    xs_old = 0.5 * ys_old
+
+    # The superset should initialize to None
+    xs_new_expected = np.array([None for _ in range(ys_new.size)])
+    xs_new_expected[2:7] = xs_old
+
+    yindex_data_old = get_yindex_data(ys_old)
+    yindex_data_new = get_yindex_data(ys_new)
+
+    xs_new = transfer_values(xs_old, yindex_data_old, yindex_data_new)
+
+    assert (np.array_equal(xs_new, xs_new_expected))
+
+def test_create_set_of_yvalues_when_getting_non_overlapping_meta_data():
+    ys_old = np.arange(9.)      # [0., 8.]
+    ys_new = 2. + np.arange(8.) # [2., 9.]
+
+    xs_old = 0.5 * ys_old
+
+    # The set cuts the first 2 value from the old and initializes unknown
+    # to None
+    xs_new_expected = np.array([None for _ in range(ys_new.size)])
+    xs_new_expected[0:7] = xs_old[2:]
+
+    yindex_data_old = get_yindex_data(ys_old)
+    yindex_data_new = get_yindex_data(ys_new)
+
+    xs_new = transfer_values(xs_old, yindex_data_old, yindex_data_new)
+
+    assert (np.array_equal(xs_new, xs_new_expected))
+
+def test_create_subset_of_yvalues_when_getting_smaller_meta_data():
+    ys_old = np.arange(9.)      # [0., 8.]
+    ys_new = 2. + np.arange(5.) # [2., 6.]
+
+    xs_old = 0.5 * ys_old
+
+    yindex_data_old = get_yindex_data(ys_old)
+    yindex_data_new = get_yindex_data(ys_new)
+
+    xs_new = transfer_values(xs_old, yindex_data_old, yindex_data_new)
+
+    assert (np.array_equal(xs_new, xs_old[2:7]))
+
+def test_create_superset_of_yvalues_initializes_with_none():
+    ys_old = None
+    ys_new = np.arange(9.)      # [0., 8.]
+
+    xs_old = None
+
+    # The superset should initialize to None
+    xs_new_expected = np.array([None for _ in range(ys_new.size)])
+
+    yindex_data_old = None
+    yindex_data_new = get_yindex_data(ys_new)
+
+    xs_new = transfer_values(xs_old, yindex_data_old, yindex_data_new)
+
+    assert (np.array_equal(xs_new, xs_new_expected))
+
+def test_create_superset_of_yvalues_works_for_no_change():
+    ys = 2. + np.arange(5.)
+    xs = 0.5 * ys
+
+    yindex_data = get_yindex_data(ys)
+
+    xs_new = transfer_values(xs, yindex_data, yindex_data)
+
+    assert (id(xs_new) == id(xs))
+
+def test_create_superset_of_yvalues_fails_if_dy_are_not_the_same():
+    ys_dy1 = np.arange(5.)
+    ys_dy2 = 2 * ys_dy1
+    xs = 0.5 * ys_dy1
+
+    yindex_data_dy1 = get_yindex_data(ys_dy1)
+    yindex_data_dy2 = get_yindex_data(ys_dy2)
+
+    with pytest.raises(ValueError) as exc:
+        transfer_values(xs, yindex_data_dy1, yindex_data_dy2)
+
+    assert ("different dy" in str(exc))
+
+
+def test_create_interface_from_ys_and_xs_values():
+    n = 10
+    ys = np.arange(n)
+    xs_left = 0.5 * ys
+    xs_right = 1.5 * ys
+
+    interface = create_interface_array(ys, xs_left, xs_right)
+
+    assert (np.array_equal(interface['Y'][:n], ys))
+    assert (np.array_equal(interface['Y'][n:], ys[::-1]))
+
+    assert (np.array_equal(interface['X'][:n], xs_left))
+    assert (np.array_equal(interface['X'][n:], xs_right[::-1]))
+
+def test_fill_pbc_info_for_y_values_picks_closest():
+    pbc_info_1 = init_periodic_info(xleft=1., xright=2.)
+    pbc_info_2 = init_periodic_info(xleft=0., xright=3.)
+
+    pbc_info_per_y = [
+        None,
+        None,
+        pbc_info_1,
+        pbc_info_2,
+        pbc_info_1,
+        pbc_info_2,
+        None,
+        None
+    ]
+
+    pbc_info_per_y = fill_pbc_info_for_empty_y_values(pbc_info_per_y)
+
+    assert (pbc_info_per_y[0] == pbc_info_1)
+    assert (pbc_info_per_y[1] == pbc_info_1)
+    assert (pbc_info_per_y[2] == pbc_info_1)
+    assert (pbc_info_per_y[3] == pbc_info_2)
+    assert (pbc_info_per_y[4] == pbc_info_1)
+    assert (pbc_info_per_y[5] == pbc_info_2)
+    assert (pbc_info_per_y[6] == pbc_info_2)
+    assert (pbc_info_per_y[7] == pbc_info_2)
+
+def test_fill_pbc_info_for_y_values_picks_default_if_none_are_found():
+    pbc_info_default = init_periodic_info()
+
+    pbc_info_per_y = [
+        None,
+        None,
+        None,
+        None
+    ]
+
+    pbc_info_per_y = fill_pbc_info_for_empty_y_values(pbc_info_per_y)
+
+    for pbc_info in pbc_info_per_y:
+        assert (pbc_info == pbc_info_default)
