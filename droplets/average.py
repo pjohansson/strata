Fix tests of averaging droplet systems

droplets/average.py
@@ -7,12 +7,12 @@ from strata.dataformats.write import write
 
 
 def average_flow_data(input_flow_maps, weights=[],
-        exclude_empty_sets=False, coord_decimals=None, coord_labels=('X', 'Y')):
+        exclude_empty_sets=False, coord_labels=('X', 'Y')):
     """Average input FlowData objects.
 
     The input data is projected onto a common coordinate grid before
     being averaged. All input data hence must have spacings set
-    and identical.
+    and identical. The data also requires the shape to be set.
 
     By default the data is averaged using an arithmetic mean. By inputting
     a list of (label, weight) tuples some data can be averaged instead
@@ -23,10 +23,9 @@ def average_flow_data(input_flow_maps, weights=[],
     or by excluding the entire set from the averaging. This is controlled
     by the `exclude_empty_sets` flag.
 
-    The coordinate vectors of all input data must be identical.
-
     Args:
-        flow_data (FlowData): List of objects to average.
+        flow_data (FlowData): List of objects to average. Must have `shape`
+            and `spacing` information set.
 
     Keyword Args:
         weights (label, weight): A list of 2-tuples with labels of data
@@ -34,9 +33,6 @@ def average_flow_data(input_flow_maps, weights=[],
 
         exclude_empty_sets (bool, optional): Whether or not to exclude empty
             data sets from the averaging process.
-
-        coord_decimals (float, optional): Use this many decimals to decide
-            grids for the averaged data.
 
         coord_labels (2-tuple, default=('X', 'Y'): Record labels for coordinates.
 
@@ -81,20 +77,13 @@ def average_flow_data(input_flow_maps, weights=[],
     except AssertionError:
         raise ValueError("No bin spacings set in FlowData input.")
 
-    #if coord_decimals:
-    if False:
-        spacing = [np.around(s, decimals=coord_decimals) for s in spacing]
-        for flow in input_flow_maps:
-            flow.data['X'] = np.around(flow.data['X'], decimals=coord_decimals)
-            flow.data['Y'] = np.around(flow.data['Y'], decimals=coord_decimals)
-
     xl, yl = coord_labels
 
     flow_data_list = [
         flow for flow in input_flow_maps
         if (exclude_empty_sets == False or flow.data.size > 0)
         ]
-        
+
     data_list = [flow.data for flow in flow_data_list]
     grid = get_combined_grid(data_list, spacing, coord_labels)
 
@@ -167,17 +156,6 @@ def average_data(data_records, weights=[], coord_labels=('X', 'Y')):
 
     xl, yl = coord_labels
 
-    # try:
-    #     assert_grids_equal(data_records)
-    # except AssertionError:
-    #     raise ValueError("Input grids of data not identical.")
-    # except IndexError:
-    #     avg_data = np.array([])
-    #     data_labels = set()
-    # else:
-    #     avg_data = data_records[0].copy()
-    #     data_labels = set(avg_data.dtype.names) - set(coord_labels)
-
     avg_data = data_records[0].copy()
     data_labels = set(avg_data.dtype.names) - set(coord_labels)
 
@@ -197,7 +175,7 @@ def average_data(data_records, weights=[], coord_labels=('X', 'Y')):
 
 
 def transfer_data(grid, data, shape, spacing, coord_labels=('X', 'Y')):
-    """Return a projection of data onto an input grid.
+    """Return a projection of `data` onto an input `grid`.
 
     The input grid must be a superset of the input data for the projection
     to succeed. Their dtype must be identical. A copy of the input grid
@@ -218,9 +196,6 @@ def transfer_data(grid, data, shape, spacing, coord_labels=('X', 'Y')):
 
     Returns:
         ndarray: Data record with projected data.
-
-    Raises:
-        ValueError: If input data or grid has coordinate duplicates.
 
     """
 
@@ -258,7 +233,8 @@ def get_combined_grid(data_list, spacing, coord_labels=('X', 'Y')):
         coord_labels (2-tuple, default=('X', 'Y'): Record labels for coordinates.
 
     Returns:
-        ndarray: A container of the same dtype as input data and of shape (ny, nx).
+        ndarray: A container of the same dtype as input data and of shape
+            (ny, nx). Will be sorted as y-major, x-minor.
 
     """
 

droplets/tests/test_average_flowdata.py
@@ -110,23 +110,36 @@ def test_combined_grid_is_created_with_same_dtype_as_input():
     assert (data.dtype == combined_grid.dtype)
     assert np.array_equal(combined_grid['C'].ravel(), np.zeros((4, )))
 
-def test_fill_data_baddata():
-    spacing = np.array([v[1] - v[0] for v in (x, y)])
-    num_samples = 10
+def test_transfer_data_from_center_bins_to_super_set_grid():
+    dtype = [('X', np.float), ('Y', np.float), ('C', np.float)]
 
-    data = gen_data(num_samples)
-    data[0] = data[1].copy()
-    combined_grid = get_combined_grid([data], spacing=spacing)
+    xgrid = np.array([0., 1., 2., 3., 4., 5.])
+    ygrid = np.array([0., 1., 2., 3., 4., 5.])
+    xs, ys = np.meshgrid(xgrid, ygrid, indexing='xy')
 
-    with pytest.raises(ValueError):
-        filled_data = transfer_data(combined_grid, data)
+    combined_grid = np.zeros((6, 6), dtype=dtype)
+    combined_grid['X'] = xs
+    combined_grid['Y'] = ys
+
+    bins = combined_grid[2:5, 3:5]
+    bins['C'] = np.random.random((3, 2))
+
+    data = transfer_data(combined_grid, bins, (2, 3), (1., 1.))
+
+    assert np.array_equal(data['X'], combined_grid['X'].ravel())
+    assert np.array_equal(data['Y'], combined_grid['Y'].ravel())
+    assert np.array_equal(data['C'].reshape(6, 6)[2:5, 3:5], bins['C'])
 
 def test_average_flow_maps():
     x = np.arange(5)
     xs, ys = np.meshgrid(x, x)
 
     num_maps = 5
-    info = {'spacing': [1, 1]}
+
+    info = {
+        'spacing': (1, 1),
+        'shape': (5, 5),
+    }
 
     f0_sets = [np.random.random(xs.shape) for _ in range(num_maps)]
     f1_sets = [np.random.random(xs.shape) for _ in range(num_maps)]
@@ -142,36 +155,15 @@ def test_average_flow_maps():
     assert np.isclose(f0_mean.ravel(), flow_mean.data['f0']).all()
     assert np.isclose(f1_mean.ravel(), flow_mean.data['f1']).all()
 
-def test_average_flow_maps_close_coords():
-    # Slightly disturb the coordinates of one set and ensure equality through
-    # an optional rounding to close values
-
-    x = np.arange(5, dtype=float)
+def test_average_flow_maps_yields_error_if_shape_of_given_flow_maps_not_set():
+    x = np.arange(5)
     xs, ys = np.meshgrid(x, x)
 
-    num_maps = 2
-    info = {'spacing': [1, 1]}
+    info = {
+        'spacing': (1, 1),
+    }
 
-    f0_sets = [np.random.random(xs.shape) for _ in range(num_maps)]
-    f1_sets = [np.random.random(xs.shape) for _ in range(num_maps)]
-    flow_maps = [FlowData(('X', xs), ('Y', ys), ('f0', f0_sets[i]), ('f1', f1_sets[i]), info=info)
-            for i in range(num_maps)]
+    flow_maps = [FlowData(('X', xs), ('Y', ys), info=info)]
 
-    f0_mean = np.average(f0_sets, axis=0)
-    f1_mean = np.average(f1_sets, axis=0, weights=f0_sets)
-
-    # Slightly perturb the values
-    flow_maps[1].data['X'] += 0.01
-    flow_maps[1].data['Y'] += 0.02
-
-    # This should cause an error since the coordinates no longer match
-    # when doing the average
-    with pytest.raises(ValueError):
-        flow_mean = average_flow_data(flow_maps, weights=[('f1', 'f0')])
-
-    # Add a keyword to round coordinates to a decimal
-    flow_mean = average_flow_data(flow_maps, weights=[('f1', 'f0')], coord_decimals=1)
-    assert np.isclose(xs.ravel(), flow_mean.data['X']).all()
-    assert np.isclose(ys.ravel(), flow_mean.data['Y']).all()
-    assert np.isclose(f0_mean.ravel(), flow_mean.data['f0']).all()
-    assert np.isclose(f1_mean.ravel(), flow_mean.data['f1']).all()
+    with pytest.raises(TypeError):
+        average_flow_data(flow_maps)